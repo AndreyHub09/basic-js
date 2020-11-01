3b0eeb66f91680fdb356916942b85278e2b98855
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2020-11-01 19:47:29 +0300

    feat: add new logic

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = '';
+  const repeatTimes = options.repeatTimes || 1;
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str;
+    const additionRepeatTimes = options.additionRepeatTimes === undefined && options.addition !== undefined ? 1 : options.additionRepeatTimes;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      result += options.addition === false || options.addition === null ? options.addition : options.addition || '';
+      if (j !== additionRepeatTimes - 1) {
+        result += options.additionSeparator || '|';
+      }
+    }
+    if (i !== repeatTimes - 1) {
+      result += options.separator || '+';
+    }
+  }
+  return result;
 };
-  
