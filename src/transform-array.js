a870342b9b3df24cdb281cd6a628affa8e7669ec
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2020-11-01 19:49:10 +0300

    feat: add new logic

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Error');
+  }
+
+  return arr.reduce((accumulator, item, index, array) => {
+
+    if (item === '--discard-prev' || item === '--double-next' || item === '--double-prev' || item === '--discard-next') {
+      switch (item) {
+        case '--discard-prev':
+          accumulator = accumulator.slice(0, accumulator.length - ((index > 1 && array[index - 2] === '--discard-next') ? 0 : 1));
+          break;
+        case '--double-next':
+          if (index !== array.length - 1) {
+            accumulator.push(array[index + 1]);
+          }
+          break;
+        case '--double-prev':
+          if (index !== 0 && !(index > 1 && array[index - 2] === '--discard-next')) {
+            accumulator.push(array[index - 1]);
+          }
+          break;
+        default:
+          break;
+      }
+      return accumulator;
+    }
+
+    if (index === 0 || typeof array[index - 1] !== 'string' || array[index - 1] !== '--discard-next') {
+      accumulator.push(item);
+    }
+
+    return accumulator;
+  }, []);
 };
