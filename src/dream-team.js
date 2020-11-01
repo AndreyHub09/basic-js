447d6eac1ebfb34e605943a7aeeaea9898fdf54c
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2020-11-01 19:47:04 +0300

    feat: add new logic

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let dreamTeam = members.reduce((accumulation, item) => {
+    if (typeof (item) !== "string") {
+      return accumulation;
+    }
+    accumulation.push(item.trim()[0].toUpperCase())
+    return accumulation;
+  }, []);
+  return dreamTeam.sort((a, b) => a > b ? 1 : -1).join('');
 };
