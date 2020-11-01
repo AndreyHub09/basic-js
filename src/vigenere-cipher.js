e1f7106d7ff9b3d3ec32b319537aa33e9a730a26
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2020-11-01 19:49:37 +0300

    feat: add new logic

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Error');
+    }
+    let result = '';
+    let spacesCount = 0;
+    for (let i = 0; i < message.length; i++) {
+      if ((/[a-zA-Z]/).test(message[i])) {
+        let keyIndex = (i - spacesCount) % key.length;
+        const messageCode = message[i].toUpperCase().charCodeAt(0);
+        const keyCode = key[keyIndex].toUpperCase().charCodeAt(0);
+        result += String.fromCharCode(((messageCode + keyCode - 130) % 26) + 65).toUpperCase();
+      } else {
+        result += message[i];
+        spacesCount++;
+      }
+    }
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error('Error');
+    }
+    let result = '';
+    let spacesCount = 0;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if ((/[a-zA-Z]/).test(encryptedMessage[i])) {
+        let keyIndex = (i - spacesCount) % key.length;
+        const encryptedMessageCode = encryptedMessage[i].toUpperCase().charCodeAt(0);
+        const keyCode = key[keyIndex].toUpperCase().charCodeAt(0);
+        result += String.fromCharCode(((encryptedMessageCode - keyCode + 26) % 26) + 65).toUpperCase();
+      } else {
+        result += encryptedMessage[i];
+        spacesCount++;
+      }
+    }
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
